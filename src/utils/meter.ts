edd32be4e909ab33f125d3a68e63dc2e73a9ea9b
Sadiq Mustapha Aji <sadiqajimustapha@gmail.com>
2025-03-22 17:50:40 +0100

    fixed app insight connection string

--- src/utils/meter.ts
+++ src/utils/meter.ts
@@ -1,7 +1,6 @@
 import { DISCO_MAP } from "@/constants/discos";
 import { Meter } from "@/types/meter.types";
 
-
 export const transformedMeters = (savedMeters: Meter[]) => {
     return savedMeters.map((meter, index) => ({
         id: meter._id,
